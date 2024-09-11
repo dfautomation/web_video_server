5b7b0a89eaf0d9f33da5a95d69caa16a8c1ad176
Patrick Chin <patrickcjh@gmail.com>
2024-09-11 13:37:39 +0800

    Revert "Add a workaround for MultipartStream constant busy state (#83)"
    This reverts commit c3d08d412f9fd960e8978233cc3e43a246bd1968.

--- src/multipart_stream.cpp
+++ src/multipart_stream.cpp
@@ -37,13 +37,10 @@ void MultipartStream::sendPartHeader(const ros::Time &time, const std::string& t
   connection_->write(async_web_server_cpp::HttpReply::to_buffers(*headers), headers);
 }
 
-void MultipartStream::sendPartFooter(const ros::Time &time) {
+void MultipartStream::sendPartFooter() {
   boost::shared_ptr<std::string> str(new std::string("\r\n--"+boundry_+"\r\n"));
-  PendingFooter pf;
-  pf.timestamp = time;
-  pf.contents = str;
   connection_->write(boost::asio::buffer(*str), str);
-  if (max_queue_size_ > 0) pending_footers_.push(pf);
+  if (max_queue_size_ > 0) pending_footers_.push(str);
 }
 
 void MultipartStream::sendPartAndClear(const ros::Time &time, const std::string& type,
@@ -52,7 +49,7 @@ void MultipartStream::sendPartAndClear(const ros::Time &time, const std::string&
   {
     sendPartHeader(time, type, data.size());
     connection_->write_and_clear(data);
-    sendPartFooter(time);
+    sendPartFooter();
   }
 }
 
@@ -63,24 +60,14 @@ void MultipartStream::sendPart(const ros::Time &time, const std::string& type,
   {
     sendPartHeader(time, type, boost::asio::buffer_size(buffer));
     connection_->write(buffer, resource);
-    sendPartFooter(time);
+    sendPartFooter();
   }
 }
 
 bool MultipartStream::isBusy() {
-  ros::Time currentTime = ros::Time::now();
-  while (!pending_footers_.empty())
+  while (!pending_footers_.empty() && pending_footers_.front().expired())
   {
-    if (pending_footers_.front().contents.expired()) {
-      pending_footers_.pop();
-    } else {
-      ros::Time footerTime = pending_footers_.front().timestamp;
-      if ((currentTime - footerTime).toSec() > 0.5) {
-        pending_footers_.pop();
-      } else {
-        break;
-      }
-    }
+    pending_footers_.pop();
   }
   return !(max_queue_size_ == 0 || pending_footers_.size() < max_queue_size_);
 }
